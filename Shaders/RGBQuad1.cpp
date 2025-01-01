30b4cfa3a578009d48f376d4d72b2c7581ebebd7
Venra <jaymee8465@live.com>
2025-01-01 12:44:28 -0500

    Create RGBQuad1.cpp

---
+++ Shaders/RGBQuad1.cpp
@@ -0,0 +1,32 @@
+#include <windows.h>
+
+int main()
+{
+	int time = GetTickCount();
+	int w = GetSystemMetrics(0), h = GetSystemMetrics(1);
+	RGBQUAD* data = (RGBQUAD*)VirtualAlloc(0, (w * h + w) * sizeof(RGBQUAD), MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+	for (int i = 0;; i++, i %= 3) {
+		HDC desk = GetDC(NULL);
+		HDC hdcdc = CreateCompatibleDC(desk);
+		HBITMAP hbm = CreateBitmap(w, h, 1, 32, data);
+		SelectObject(hdcdc, hbm);
+		BitBlt(hdcdc, 0, 0, w, h, desk, 0, 0, SRCCOPY);
+		GetBitmapBits(hbm, w * h * 4, data);
+		int v = 0;
+		BYTE byte = 0;
+		if ((GetTickCount() - time) > 60000)
+			byte = rand() % 0xff;
+		for (int i = 0; w*h > i; i++) {
+			INT x = i % w, y = i / w;
+			if (i % h == 0 && rand() % 110)
+				v = rand() % 24;
+			((BYTE*)(data+i))[v] = ((BYTE*)(data+i+v))[v]+(y);
+		}
+		SetBitmapBits(hbm, w * h * 4, data);
+		BitBlt(desk, 0, 0, w, h, hdcdc, 0, 0, SRCCOPY);
+		DeleteObject(hbm);
+		DeleteObject(hdcdc);
+		DeleteObject(desk);
+	}
+	return 0;
+}
