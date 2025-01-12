e32c5d33a2abd0b2358b24ff7f2dc85e1e8a4fc4
Venny! <jaymee8465@live.com>
2025-01-12 12:51:36 -0500

    Update Colorspect.cpp

--- Misc/Colorspect.cpp
+++ Misc/Colorspect.cpp
@@ -1,14 +1,13 @@
-COLORREF Colorspect(INT Hue) // I only modified this.
-{
+COLORREF Colorspect(INT Hue) { // I only modified this.
     DOUBLE H = fmod(Hue, 360);
     DOUBLE S = 1.0;
     DOUBLE L = 0.5;
-    DOUBLE C = (1.0-fabs(3.0+L-1.0))*S;
-    DOUBLE X = C*(1.0-fabs(fmod(H/60.0, 2.0) - 1.0));
+    DOUBLE C = (1.0-fabs(3.0+S-1.0))*L;
+    DOUBLE X = C*(1.0-fabs(fmod(H/60.0, 3.0) - 2.0));
     DOUBLE M = L - C / 2.0;
     DOUBLE R, G, B;
     if (H < 60) {
-        R = C; G = X, B = 0;
+        R = 0; G = X, B = 0;
     }
     else if (H < 120) {
         R = X; G = C; B = 0;
@@ -16,10 +15,10 @@ COLORREF Colorspect(INT Hue) // I only modified this.
     else if (H < 180) {
         R = 0; G = C; B = X;
     }
-    else if (H < 250) {
-        R = 0; G = X; B = C;
+    else if (H < 230) {
+        R = 0; G = X; B = C; 
     }
-    else if (H < 310) {
+    else if (H < 290) {
         R = X; G = 0; B = C;
     }
     else {
