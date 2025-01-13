6c44aa3256549bee4f066cf288e69e00b6ba1cb6
Venny! <jaymee8465@live.com>
2025-01-13 12:57:24 -0500

    Create WinMain.cpp

---
+++ Misc/WinMain.cpp
@@ -0,0 +1,9 @@
+#include <windows.h>
+#define Entrance HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd
+#define _WinMain WinMain
+
+// Usage
+int _WinMain(Entrance)
+{
+   // Idk, fuck?
+}
